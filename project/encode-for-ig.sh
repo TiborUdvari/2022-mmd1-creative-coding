f31876d0ef7a3b0a1e277dd981af53b313496594
Tibor Udvari <tibor.udvari@icloud.com>
2023-02-15 11:49:06 +0100

    Added audio filters to encoding

--- project/encode-for-ig.sh
+++ project/encode-for-ig.sh
@@ -7,6 +7,11 @@
 # Loop it, apply fades
 # Convert to IG format
 
+# todo
+# Filter the audio to remove annoying sounds
+# ffmpeg -i 2023-02-14_22-59-27-e3b90f4.wav -af "highpass=f=20,afftdn=nf=-20,lowpass=f=5000"
+# Add a normalisation to it maybe
+
 # The audio should be a .wav file with the same name as the video
 export PATH=$PATH:/usr/local/bin/
 
@@ -19,7 +24,8 @@ echo "Script dir is $script_dir"
 FILE_NAME="$1"
 VIDEO_FILE=$FILE_NAME.avi
 ffmpeg -y -r 60 -pattern_type glob -i '*.png' -c:v huffyuv -pix_fmt yuv444p $FILE_NAME-mute.avi
-ffmpeg -y -i $FILE_NAME-mute.avi -i $FILE_NAME.wav -c:v copy -c:a copy $VIDEO_FILE
+ffmpeg-normalize -t -25 $FILE_NAME.wav -o $FILE_NAME-norm.wav
+ffmpeg -y -i $FILE_NAME-mute.avi -i $FILE_NAME-norm.wav -c:v copy -c:a copy $VIDEO_FILE
 
 VIDEO_FILE_NAME=$(basename "$VIDEO_FILE")
 VIDEO_FILE_NAME="${VIDEO_FILE_NAME%.*}"
@@ -27,8 +33,7 @@ VIDEO_FILE_EXT="${VIDEO_FILE##*.}"
 VIDEO_FILE_DIR=$(dirname "$VIDEO_FILE")
 LOOPED_VIDEO_FILE=$VIDEO_FILE_NAME-looped.$VIDEO_FILE_EXT
 
-TARGET_LENGTH=15
-echo "Got here";
+TARGET_LENGTH=10
 
 LENGTH=$(ffprobe -i "$VIDEO_FILE" -show_entries format=duration -v quiet -of csv="p=0")
 LOOP_COUNT=$(echo "$TARGET_LENGTH/$LENGTH" | bc)
@@ -42,7 +47,7 @@ AFADE_OUT_START=$(echo "$LOOPED_VIDEO_FILE_LENGTH-1" | bc)
 
 ffmpeg -y -i $LOOPED_VIDEO_FILE \
     -vf "fade=t=in:st=0:d=1,fade=t=out:st=$FADE_OUT_START:d=2" \
-    -af "afade=t=in:st=0.25:d=1,afade=t=out:st=$AFADE_OUT_START:d=1" \
+    -af "highpass=f=20,afftdn=nf=-20,lowpass=f=5000,afade=t=in:st=0.25:d=1,afade=t=out:st=$AFADE_OUT_START:d=1" \
     -c:a libfdk_aac -profile:a aac_he_v2 -b:a 256k \
     -c:v libx264 -pix_fmt yuv420p -crf 2 -preset veryslow -tune animation -f mp4 \
     $VIDEO_FILE_DIR/$VIDEO_FILE_NAME-looped-faded.mov
